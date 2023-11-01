407b3b0975596caaebe96603b8a429371be67e75
K4naxa <k4na@iki.fi>
2023-11-01 18:22:20 +0200

    created the chessboard with js

---
+++ script.js
@@ -0,0 +1,20 @@
+const placeKnightBtn = document.querySelector(".placeKnightBtn");
+const boardContainer = document.querySelector(".boardContainer");
+
+function createBoard() {
+  for (let y = 7; y >= 0; y--) {
+    for (let x = 0; x < 8; x++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      boardContainer.appendChild(cell);
+      cell.innerHTML = `${x}, ${y}`;
+
+      if ((x % 2 == 0 && y % 2 == 0) || (x % 2 != 0 && y % 2 != 0)) {
+        cell.style.backgroundColor = "black";
+        cell.style.color = "white";
+      }
+    }
+  }
+}
+
+createBoard();
